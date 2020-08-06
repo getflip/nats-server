74f8675c0e82148e6269e55c6408f3b203749d03
Heiko Nickerl <dev@heiko-nickerl.com>
2020-08-06 15:21:49 +0200

    Make jwks cache thread safe

--- server/jwks.go
+++ server/jwks.go
@@ -14,6 +14,7 @@ type Keycloak struct {
 	server    *Server
 	host      string
 	jwksCache map[string]*jwk.Set
+	mutex     *sync.Mutex
 }
 
 var once sync.Once
@@ -25,6 +26,7 @@ func KeycloakInstance(server *Server) *Keycloak {
 		keycloak.server = server
 		keycloak.host = keycloakHost()
 		keycloak.jwksCache = make(map[string]*jwk.Set)
+		keycloak.mutex = &sync.Mutex{}
 	})
 	return keycloak
 }
@@ -43,20 +45,27 @@ func (keycloak *Keycloak) publicKey(token *jwt.Token, retry bool) (interface{}, 
 
 	claims := token.Claims.(jwt.MapClaims)
 	realm := claims["tenant"]
-	keycloak.server.Debugf("Verifying token for tenant '%s'\n", realm)
+	keycloak.server.Debugf("Verifying token for tenant '%s'", realm)
 
 	jwksURL := fmt.Sprintf("http://%s/auth/realms/%s/protocol/openid-connect/certs", keycloak.host, realm)
+
+	keycloak.mutex.Lock()
 	jwks, exists := keycloak.jwksCache[jwksURL]
+	keycloak.mutex.Unlock()
+
 	if exists {
-		keycloak.server.Debugf("Using JWKS from cache (realm '%s')\n", realm)
+		keycloak.server.Debugf("Using JWKS from cache (realm '%s')", realm)
 	} else {
-		keycloak.server.Debugf("Fetching JWKS (realm '%s')\n", realm)
+		keycloak.server.Debugf("Fetching JWKS (realm '%s')", realm)
 		jwksFetch, err := jwk.FetchHTTP(jwksURL)
 		if err != nil {
+			keycloak.server.Errorf("Failed to fetch JWKS (realm '%s)", realm)
 			return nil, err
 		}
 		jwks = jwksFetch
+		keycloak.mutex.Lock()
 		keycloak.jwksCache[jwksURL] = jwks
+		keycloak.mutex.Unlock()
 	}
 
 	if key := jwks.LookupKeyID(keyID); len(key) == 1 {
@@ -65,7 +74,9 @@ func (keycloak *Keycloak) publicKey(token *jwt.Token, retry bool) (interface{}, 
 		if retry {
 			// current jwks does not match the tokens kid, the servers jwks might have changed
 			keycloak.server.Warnf("JWKS (realm '%s') doesn't match given token, trying to refetch jwks", realm)
+			keycloak.mutex.Lock()
 			delete(keycloak.jwksCache, jwksURL)
+			keycloak.mutex.Unlock()
 			return keycloak.publicKey(token, false)
 		} else {
 			keycloak.server.Errorf("Newly fetched JWKS (realm '%s') doesn't match given token, giving up", realm)
